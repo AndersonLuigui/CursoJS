af862e98abe5a3ae526f46a1c56c9c6c1fab6acd
AndersonLuigui <alns1536@hotmail.com>
2023-01-12 19:51:05 -0300

    exercícios

---
+++ aula10 - Operadores Aritiméticos/index.js
@@ -0,0 +1,33 @@
+/* Operadores aritiméticos 
+
++ = ADIÇÃO E CONTATENAÇÃO  -  CONTATENAÇÃO CONSISTE EM JUNTAR OS VALORES STRINGS
+- = SUBTRAÇÃO
+* = MULTIPLICAÇÃO
+/ = DIVISÃO
+** = POTENCIAÇÃO
+% = RESTO DA DIVISÃO
+++ = OPERADORES DE INCREMENTO
+-- = OPERADORES DE DECREMENTO
++= = OPERADORES DE ATRIBUIÇÃO - FUNCIONA COM TODOS OS OPERADORES
+
+
+
+ */
+
+const num1 = 5;
+const num2 = 10;
+console.log(num1 ** num2);
+
+
+
+let contador = 1;
+contador++;
+console.log(contador);
+
+
+// O parseInt, parseFloat... FAZ A CONVERSÃO DOS VALORES - O Number CONVERTE PARA NÚMERO.
+
+const num3 = 10;
+const num4 = parseInt ('5');
+console.log(num4);
+
---
+++ aula11/index.js
@@ -0,0 +1,8 @@
+
+let num1 = prompt("Digite o primeiro numero ");
+let num2 = prompt("Digite o segundo numero ");
+num1 = Number(num1);
+num2 = Number(num2);
+
+
+alert(`O resultado é igual à ${num1 + num2}`);
