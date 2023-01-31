0044cfd76c820fd37e00fbc8e5983ea90ea68534
Anderson Luigui <alns1536@hotmail.com>
2023-01-31 13:08:10 -0300

    aula 14

---
+++ aula15 - objeto Math/index.js
@@ -0,0 +1,29 @@
+let num1 = 9.54578;
+let num2 = Math.floor(num1); // Math.floor ARREDONDA O VALOR PARA BAIXO
+console.log(num2);
+
+
+
+let num3 = 9.54579;
+let num4 = Math.ceil(num3); // Math.ceil ARREDONDA PARA CIMA
+console.log(num4);
+
+
+
+let num5 = 9.50;
+let num6 = Math.round(num5);
+console.log(num2);
+
+
+console.log(Math.max(1,3,5,8,11,-50,1500,45,300)); // O COMANDO Math.max() PEGA O MAIOR NÚMERO DA SEQUENCIA
+console.log(Math.min(1,3,5,8,11,-50,1500,45,300)); // O COMANDO Math.min() PEGA O MENOR NÚMERO DA SEQUENCIA
+
+console.log(Math.random()); // GERA UM NÚMERO ALEATÓRIO 
+
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5);  // GERA UM NÚMERO ALEATÓRIO ENTRE 10 E 5
+console.log(aleatorio);
+
+
+ 
+
+
