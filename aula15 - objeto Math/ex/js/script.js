5af89dfd0941a84dba1b9f6031846355d6b75a91
Anderson Luigui <alns1536@hotmail.com>
2023-01-31 16:59:57 -0300

    Exercícios Aula 15

---
+++ aula15 - objeto Math/ex/js/script.js
@@ -0,0 +1,18 @@
+const numero = Number(prompt("Digite um nm número"));
+const numeroTitulo = document.getElementById('numero-titulo');
+const texto = document.getElementById('texto');
+
+
+numeroTitulo.innerHTML = numero;
+texto.innerHTML + '';
+texto.innerHTML =+ `<p>O seu número é ${numero}</p>`;
+texto.innerHTML =+ `<p>A raiz quadrada do número ${numero} é ${Math.sqrt(numero)}</p>`;
+texto.innerHTML =+ `<p>${numero} é inteiro: ${Number.isInteger(numero)}</p>`;
+texto.innerHTML =+ `<p>NaN é ${Number.isNaN(numero)}</p>`;
+texto.innerHTML =+ `<p>Arredondando para baixo ${Math.floor(numero)}</p>`;
+texto.innerHTML =+ `<p>Arredondando para cima ${Math.ceil(numero)}</p>`;
+texto.innerHTML =+ `<p>Em duas casas decimais ${numero.toFixed(2)}</p>`;
+
+
+
+
--- aula15 - objeto Math/index.js
+++ aula15 - objeto Math/index.js
@@ -11,14 +11,14 @@ console.log(num4);
 
 
 let num5 = 9.50;
-let num6 = Math.round(num5);
+let num6 = Math.round(num5); // Math.round ARREDONDA DE FORMA AUTOMÁTICA
 console.log(num2);
 
 
 console.log(Math.max(1,3,5,8,11,-50,1500,45,300)); // O COMANDO Math.max() PEGA O MAIOR NÚMERO DA SEQUENCIA
 console.log(Math.min(1,3,5,8,11,-50,1500,45,300)); // O COMANDO Math.min() PEGA O MENOR NÚMERO DA SEQUENCIA
 
-console.log(Math.random()); // GERA UM NÚMERO ALEATÓRIO 
+console.log(Math.random()); // GERA UM NÚMEROS ALEATÓRIO 
 
 const aleatorio = Math.round(Math.random() * (10 - 5) + 5);  // GERA UM NÚMERO ALEATÓRIO ENTRE 10 E 5
 console.log(aleatorio);
