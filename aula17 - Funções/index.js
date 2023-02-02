16c1946642c6be57ce113448c5d738461ac347f8
Anderson Luigui <alns1536@hotmail.com>
2023-02-02 17:10:50 -0300

    Aula 17 - Funções

---
+++ aula17 - Funções/index.js
@@ -0,0 +1,20 @@
+//function soma(x, y) {
+ //   const resultado = x + y;
+ // return resultado;
+//}
+
+//resultado = soma();
+// console.log(soma(2, 2));
+
+const raiz = function(n) {
+    return n ** 0.5;
+
+};
+
+
+// OUTRA MANEIRA DE COLOCAR UMA FUNÇÃO
+const outraRaiz = n => n ** 0.5; 
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(36));
