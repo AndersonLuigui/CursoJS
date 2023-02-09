729d43286869c232be111a6120c95d2c381d22c4
Anderson Luigui <alns1536@hotmail.com>
2023-02-09 13:59:29 -0300

    Aula 21 OPERADORES DE COMPARAÇÃO

--- aula21/index.js
+++ aula21/index.js
@@ -1,12 +1,17 @@
-/* OPERADORES DE COMPARAÇÃO 
+/* OPERADORES DE COMPARAÇÃO ||||||||||||||||||||||||||||||||||||||||||||||||||||| OPERADORES DE COMPARAÇÃO ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
 > MAIOR QUE
 >= MAIOR OU IGUAL
 < MENOR QUE
 <= MENOR OU IGUAL
-== IGUALDADE (VALOR)
-=== IGUALDADE ESTRITA (VALOR E TIPO)
+=== IGUALDADE ESTRITA CHECA (VALOR E TIPO)
 != DIFERENTE (VALOR)
 !== DIFERENTE ESTRITA (VALOR E TIPO)
 
 */
+
+const num1 = 10;
+const num2 = 10;
+
+const comp = num1 != num2;
+console.log(comp);
