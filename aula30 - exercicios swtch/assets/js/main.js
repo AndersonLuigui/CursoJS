981a2f1122a37a130216dd45a80a9361a3d12a19
Anderson Luigui <alns1536@hotmail.com>
2023-03-09 14:43:02 -0300

    Exercícios com switch

--- aula30 - exercicios swtch/assets/js/main.js
+++ aula30 - exercicios swtch/assets/js/main.js
@@ -1,87 +1,24 @@
-/* // dia da semana + numero do dia + nome do mês + ano
+// dia da semana + numero do dia + nome do mês + ano
 
 const h1 = document.querySelector('.container h1');
 const data = new Date;
 h1.innerHTML = data.toString();
 
 function getDiaSemanaTexto (diaSemana) {
-    let diaSemanaTexto;
-    switch (diaSemana) {
-    case 0:
-        diaSemanaTexto = 'domingo';
-        return diaSemanaTexto;
-    case 1:
-        diaSemanaTexto = 'segunda-feira';
-        return diaSemanaTexto;
-    case 2:
-        diaSemanaTexto = 'terça-feira';
-        return diaSemanaTexto;
-    case 3:
-        diaSemanaTexto = 'quarta-feira';
-        return diaSemanaTexto;
-    case 4:
-        diaSemanaTexto = 'quinta-feira';
-        return diaSemanaTexto;
-    case 5:
-        diaSemanaTexto = 'sexta-feira';
-        return diaSemanaTexto;
-    case 6:
-        diaSemanaTexto = 'sábado';
-        return diaSemanaTexto;
-    default:
-        diaSemanaTexto = '';
-        return diaSemanaTexto;
-    }
-
+    const diasSemana = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado'];
+    return diasSemana[]; 
 }
 
 
 function getNomeMes(numeroMes) {
-    let nomeMes;
+    const meses = ['janeiro', 'fevereiro', 'março','abril', 'maio','junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+    return meses[numeroMes];
 
-    switch (numeroMes) {
-    case 0:
-        nomeMes = 'janeiro';
-        return nomeMes;
-    case 1:
-        nomeMes = 'fevereiro';
-        return nomeMes;
-    case 2:
-        nomeMes = 'março';
-        return nomeMes;
-    case 3:
-        nomeMes = 'abril';
-        return nomeMes;
-    case 4:
-        nomeMes = 'maio';
-        return nomeMes;
-    case 5:
-        nomeMes = 'junho';
-        return nomeMes;
-    case 6:
-        nomeMes = 'julho';
-        return nomeMes;
-    case 7:
-        nomeMes = 'agosto';
-        return nomeMes;
-    case 8:
-        nomeMes = 'setembro';
-        return nomeMes;
-    case 9:
-        nomeMes = 'outubro';
-        return nomeMes;
-    case 10:
-        nomeMes = 'novembro';
-        return nomeMes;
-    case 11:
-        nomeMes = 'dezembro';
-        return nomeMes;
-    }
 
 }
 
 
-function criaData(data) {
+/* function criaData(data) {
     const diaSemana = data.getDay();
     const numeroMes = data.getMonth();
     const nomeDia = getDiaSemanaTexto(diaSemana);
@@ -93,18 +30,16 @@ function criaData(data) {
 
     );
 
-}
+} */
 
-h1.innerHTML = criaData(data); */
+h1.innerHTML = criaData(data);
 
 
 
 // AGORA TODO ESTE CÓDIGO EM CIMA PODE SER RESUMIDO EM POUCAS LINHAS COMO ABAIXO >>
 
 /* const h1 = document.querySelector('.container h1');
-const data = new Date;
-/* const opcoes = {
-    dateStyle: 'full'
-}; 
+const data = new Date();
 
-h1.innerHTML = data.toLocaleDateString('pt-BR', { dateStyle: 'full'}); */
+
+h1.innerHTML = data.toLocaleDateString('pt-BR', { dateStyle: 'full', timeStyle: 'short'});  */
