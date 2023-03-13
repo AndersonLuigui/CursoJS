d07d1b2331ac848cc68348a249efbbb88a314e84
Anderson Luigui <alns1536@hotmail.com>
2023-03-13 10:44:31 -0300

    Mais diferenças entre Let e Var

---
+++ aula31 - let & var/index.js
@@ -0,0 +1,52 @@
+const verdadeira = true;
+
+/* let nome = 'Anderson';
+var nome2 = 'Anderson';
+
+// LET TEM ESCOPO DE BLOCO { . . . }
+// VAR SÓ TEM ESCOPO DE FUNÇÃO
+
+if (verdadeira) {
+    let nome = 'Luigui'; // CRIANDO OUTRA VAR LET DENTRO DO BLOCO
+/*     console.log(nome, nome2); */
+
+
+/*     if  (verdadeira) {
+        let nome = 'Outro nome';
+        var nome
+        console.log(nome,nome2);
+    }
+
+
+
+} */
+
+
+
+
+
+
+// O ESCOPO DA FUNÇÃO É ESPECIAL QUANDO UMA VAR É DEFINIDA DENTRO DO ESCOPO DA FUNÇÃO ELA FICA PROTEGIDA 
+// JÁ UMA VAR DEFINIDA FORA DO ESCOPO DA FUNÇÃO OU SEJA NO ESCOPO GLOBAL
+// ELA PODE SER ACESSADA DENTRO DA FUNÇÃO - TAMBÉM É POSSÍVEL ESCREVER MINI ESCOPOS COM IF/ELSE DENTRO DA FUNÇÃO
+
+
+/* function falaOi () {
+    var sobrenome = 'Luigui';
+
+    if (verdadeira) {
+        let nome = 'Anderson';
+        console.log(sobrenome);
+    
+    } 
+
+    console.log(sobrenome);
+
+}
+
+
+falaOi(); */
+
+console.log(sobrenome);
+
+let sobrenome = 'Miranda';
