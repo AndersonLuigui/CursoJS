6e57534306186e3c290fedcf7f35cfcb0a343eb1
AndersonLuigui <alns1536@hotmail.com>
2023-03-14 20:47:40 -0300

    Aula 33 finalizada

--- aula33 - Atribuição por desestruturação objeto/index.js
+++ aula33 - Atribuição por desestruturação objeto/index.js
@@ -8,4 +8,14 @@ const pessoa = {
     }
 };
 
-console.log(pessoa);
+// const nome = pessoa.nome; // ATRIBUIÇÃO NORMAL
+
+/* 
+const { nome, sobrenome, idade, endereco } = pessoa; // ATRIBUIÇÃO POR DESESTRUTURAÇÃO
+console.log(nome, sobrenome, endereco);  */
+/* 
+const { endereco: { rua, numero}, endereco } = pessoa; // EXTRAINDO O ENDEREÇO DO OBJETO PESSOA
+console.log(rua, numero);  */
+
+const { endereco: { rua: r, numero}, endereco } = pessoa; // ATRIBUINDO O VALOR DE RUA PARA R - MUDANDO O A VARIÁVEL
+console.log(r, numero); 
