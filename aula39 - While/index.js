81d203898aa2d55b9bbd0e54de27647c05db59be
Anderson Luigui <alns1536@hotmail.com>
2023-03-21 14:06:20 -0300

    aula 39 While

---
+++ aula39 - While/index.js
@@ -0,0 +1,24 @@
+function random(min, max) {
+    const r = Math.random() * (max - min) + min;
+    return Math.floor(r);
+}
+
+
+const min = 1;
+const max = 50;
+let rand = random(min, max);
+
+//WHILE CHECA A CONDIÇÃO E EXECUTA O CÓDIGO - ENQUANTO TRUE FAÇA ATÉ QUE SEJA FALSE
+while (rand !== 10) {
+    rand = random(min, max);
+    console.log(rand);
+}
+
+//_______________________________________________________________________________
+
+// DO WHILE CHECA  A CONDIÇÃO POR ÚLTIMO - FAÇA ISSO EM SEGUIDA CHEQUE A CONDIÇÃO
+do {
+    rand = random(min, max);
+    console.log(rand);
+} while (rand !== 10);
+
