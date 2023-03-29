cfa6c2524771c2ce9f745d32ab6905605a9e0fe7
Anderson Luigui <alns1536@hotmail.com>
2023-03-29 12:00:42 -0300

    Exercício Fizz Buzz

--- aula43 - Exercícios Fizz Buzz/index.js
+++ aula43 - Exercícios Fizz Buzz/index.js
@@ -5,7 +5,22 @@ NÚMERO É DIVISÍVEL POR 3 = FIZZ
 NÚMERO É DIVISÍVEL POR 5 = BUZZ
 NÚMERO É DIVISÍVEL POR 3 E 5 = FIZZBUZZ
 NÚMERO NÃO É DIVISÍVEL POR 3 E 5 = RETORNA O PRÓPRIO NÚMERO
-CHECAR SE O NÚMERO É REALMENTE UM NÚMERO
+CHECAR SE O NÚMERO É REALMENTE UM NÚMERO = RETORNA O PRÓPRIO NÚMERO
 USE A FUNÇÃO COM NÚMEROS DE 0 A 100
 
 */
+ 
+function fizzBuzz(numero) {
+    if (typeof numero !== 'number') return numero;
+    if (numero % 3 === 0 && numero % 5 === 0) return 'FizzBuzz';
+    if (numero % 3 === 0) return 'Fizz';
+    if (numero % 5 === 0) return 'Buzz';
+    return numero
+}
+
+
+console.log('a',fizzBuzz('a'));
+
+for (let i = 0; i <= 100; i++) {
+    console.log(i,fizzBuzz(i));
+}
