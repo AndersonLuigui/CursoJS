9d19f2c948228f755f4327908d72784dee2aa4c7
ANDERSON LUIGUI <alns1536@hotmail.com>
2023-04-23 22:40:29 -0300

    Aula 51 e 52

---
+++ aula52 - Escopo Lexico/aula52.js
@@ -0,0 +1,18 @@
+const nome = 'Anderson';
+
+function falaNome() {
+
+    console.log(nome)
+}
+
+falaNome();
+
+
+function usaFalaNome() {
+    const nome = 'Luigui';
+    falaNome();
+}
+
+usaFalaNome();
+
+// TENDI FOI NADA
